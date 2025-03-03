335a2c8b8bc6d9614f8ac1fbb81b7614cb06f145
phiquangdat <datphi020603@gmail.com>
2025-03-03 16:22:52 +0200

    passing props to components

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,10 +1,16 @@
 import List from "./List";
+import Card from "./Card";
+import { useState } from "react";
 export default function Board({ listName }) {
+  const [item, setItem] = useState("");
+
+  function handleClick(e) {}
   return (
     <div className="list">
       <h3>{listName}</h3>
       <ul>
-        <li>Ta</li>
+        <Card>{item}</Card>
+        <button onClick={handleClick}>Add Task</button>
       </ul>
     </div>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
-export default function Card({ item }) {
+export default function Card({ children }) {
   return (
     <>
-      <li>{item}</li>
+      <li>{children}</li>
     </>
   );
 }
