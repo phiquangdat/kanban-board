34b1efece9ec8d7ad5b790938d392eccd51d9d98
phiquangdat <datphi020603@gmail.com>
2025-03-06 09:29:12 +0200

    move the add task button

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { visualizer } from "rollup-plugin-visualizer";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
